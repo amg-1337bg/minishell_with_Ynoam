cd2930470bc22a9f787dbf41370d88efaf6fdf50
yssefnoam <2020noam@gmail.com>
2021-03-06 11:09:50 +0100

    a

--- executing/execute.c
+++ executing/execute.c
@@ -6,7 +6,7 @@
 /*   By: ynoam <ynoam@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 13:30:36 by ynoam             #+#    #+#             */
-/*   Updated: 2021/03/05 15:06:48 by ynoam            ###   ########.fr       */
+/*   Updated: 2021/03/05 22:32:58 by ynoam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 int     execute(t_cmd *cmds, char **env)
 {
+    // pipe
+    // fork
+    // if parent then wait
+    // else execve then exit
     return (0);
 }
