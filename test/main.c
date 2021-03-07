f0049f0ff4158917281f96b51b0a124833cf4c2d
yssefnoam <2020noam@gmail.com>
2021-03-07 19:26:18 +0100

    aa

--- test/main.c
+++ test/main.c
@@ -11,24 +11,34 @@ int main(int argc, char *argv[], char *env[])
     int i = 0;
     while (i != 2)
     {
+        int fd[2];
+        if (thereIsAPipe == 1)
+        {
+            pipe(fd);
+        }
         pid = fork();
         if (pid == 0) // child process
         {
             if (thereIsAPipe == 1)
             {
-                int fd[2];
-                pipe(fd);
+                dup2(fd[1], STDOUT_FILENO);
                 close(fd[0]);
+                close(fd[1]);
             }
-            char *args[]={"/bin/ls", "lklkjlklkj",NULL}; 
+            else
+            {
+                dup2(fd[0], STDIN_FILENO);
+                close(fd[0]);
+                close(fd[1]);
+            }
+
+            char *args[]={"/bin/ls", "./",NULL}; 
             execv(args[0],args); 
 
         }
-        else // parent process
-        {
-            waitpid(pid, &returnValue, 0);
-            i++;
-        }
+        waitpid(pid, &returnValue, 0);
+        i++;
+
     }
     if (returnValue == 256)
         returnValue = 1;
