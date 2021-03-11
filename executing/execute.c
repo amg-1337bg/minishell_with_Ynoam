c3b97c581be769e8b42a6526a0ba220482c36f29
Youssef Noam <ynoam@e1r12p6.1337.ma>
2021-03-11 18:53:36 +0100

    a

--- executing/execute.c
+++ executing/execute.c
@@ -12,43 +12,18 @@
 
 #include "../parsing/minishell.h"
 
-int     how_much_pipes(t_cmd *cmds)
-{
-        int i;
 
-        i = 0;
-        while(cmds->pipe == 1)
-            i++;
-        return (i);
+void handle_sigint(int sig) 
+{ 
+    printf("Caught signal %d\n", sig); 
+    exit(0);
 }
-
 void    ft_creat_file(char *filename)
 {
     open(filename, O_WRONLY |  O_CREAT | O_TRUNC, 00644);
 }
 int     execute(t_cmd *cmds, char **env)
 {
-        // how_much_semicolon(cmds);
-        // while(1)
-        // {
-        //     int i;
-        //     i = how_much_pipes(cmds);
-        //     int fd[i][2];
-            
-        //     while(i--)
-        //     {
-
-        //     }
-
-        // }
-        
-
-
-
-
-
-
-
 
 
 
