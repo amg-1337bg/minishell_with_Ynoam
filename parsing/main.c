6f339c7aebd7e373aa0e607d2374d787bc6cdf8b
amg-1337bg <maradona551@hotmail.fr>
2021-06-08 19:51:36 +0100

    leaks

--- parsing/main.c
+++ parsing/main.c
@@ -38,9 +38,9 @@ int	main(int argc, char **argv, char **envp)
 		if (get_cmd(&s_cmd, s_env, line, &cmd_return) == 0)
 			cmd_return = execute(s_cmd, s_env, cmd_return);
 		// if (argv[2])
-		// 	exit(0);
+		// 	exit(cmd_return);
 		free_cmd(&s_cmd);
 		free(line);
 	}
-	return (0);
+	return (cmd_return);
 }
