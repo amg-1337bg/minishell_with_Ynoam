6024b5fac7d1382ba349cf579139d170ead20161
Youssef Noam <ynoam@e1r12p5.1337.ma>
2021-06-11 11:22:50 +0100

    fix error

--- builtin/echo.c
+++ builtin/echo.c
@@ -40,7 +40,7 @@ int	ft_echo(char *argv[], int *fd)
 			j = 1;
 			while (argv[i][j] == 'n')
 				j++;
-			if (argv[i][j] == '\0' && ++i && ++flag)
+			if (argv[i][j] == '\0' && ++flag)
 				continue ;
 			if (argv[i][j] != '\0')
 				break ;
